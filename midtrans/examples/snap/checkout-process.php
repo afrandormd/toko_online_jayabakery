ed5268bbe7ff0562d55eaf57d4e643afcbc76edb
Afrando Sharein Ramadhan <afrandormd@gmail.com>
2024-06-06 01:32:33 +0700

    fix akumulasi pemesanan yang sudah di bayar

--- midtrans/examples/snap/checkout-process.php
+++ midtrans/examples/snap/checkout-process.php
@@ -22,38 +22,45 @@ Config::$is3ds = true;
 // Config::$appendNotifUrl = "https://example.com";
 // Config::$overrideNotifUrl = "https://example.com";
 
-//koneksi ke database
+// Koneksi ke database
 include "../../../koneksi/koneksi.php";
 $kd_cs = $_GET['kd_cs'];
 
 // Ambil data transaksi dari database
-$transaction_query = mysqli_query($conn, "SELECT * FROM produksi WHERE kode_customer = '$kd_cs'");
+$transaction_query = mysqli_query($conn, "SELECT * FROM produksi WHERE kode_customer = '$kd_cs' AND terima != 1 AND tolak != 1");
+
+if (!$transaction_query) {
+    die("Error querying database: " . mysqli_error($conn));
+}
 
 $gross_amount = 0;
 $item_details = [];
 while ($row = mysqli_fetch_assoc($transaction_query)) {
-    $item_details[] = array(
-        'id' => $row['kode_produk'],
-        'price' => $row['harga'],
-        'quantity' => $row['qty'],
-        'name' => $row['nama_produk']
-    );
-    $gross_amount += $row['harga'] * $row['qty'];
+    if ($row['harga'] > 0 && $row['qty'] > 0) {
+        $item_details[] = array(
+            'id' => $row['kode_produk'],
+            'price' => $row['harga'],
+            'quantity' => $row['qty'],
+            'name' => $row['nama_produk']
+        );
+        $gross_amount += $row['harga'] * $row['qty'];
+    } else {
+        echo "Invalid product data: ";
+        print_r($row);
+    }
 }
 
 // Pastikan gross_amount valid
 if ($gross_amount < 0.01) {
-    die("Total amount must be at least 0.01");
+    die("Total amount must be at least 0.01. Gross amount: $gross_amount");
 }
 
-// Ambil detail transaksi pertama sebagai contoh
-mysqli_data_seek($transaction_query, 0); // Reset pointer untuk mengambil data pertama
-$transaction_details_row = mysqli_fetch_assoc($transaction_query);
+// Generate unique order ID
+$unique_order_id = uniqid($kd_cs . '-');
 
 // Detail transaksi
 $transaction_details = array(
-    // 'order_id' => $transaction_details_row['invoice'],
-    'order_id' => $transaction_details_row['id_order'],
+    'order_id' => $unique_order_id,
     'gross_amount' => $gross_amount, // no decimal allowed for creditcard
 );
 
@@ -67,18 +74,18 @@ $customer_details = array(
     'phone' => isset($customer_data_details['telp']) ? $customer_data_details['telp'] : '',
     'billing_address' => array(
         'first_name' => isset($customer_data_details['nama']) ? $customer_data_details['nama'] : '',
-        'address' => isset($transaction_details_row['alamat']) ? $transaction_details_row['alamat'] : '',
-        'city' => isset($transaction_details_row['kota']) ? $transaction_details_row['kota'] : '',
-        'postal_code' => isset($transaction_details_row['kopos']) ? $transaction_details_row['kopos'] : '',
-        'phone' => isset($transaction_details_row['phone']) ? $transaction_details_row['phone'] : '',
+        'address' => isset($customer_data_details['alamat']) ? $customer_data_details['alamat'] : '',
+        'city' => isset($customer_data_details['kota']) ? $customer_data_details['kota'] : '',
+        'postal_code' => isset($customer_data_details['kopos']) ? $customer_data_details['kopos'] : '',
+        'phone' => isset($customer_data_details['phone']) ? $customer_data_details['phone'] : '',
         'country_code' => 'IDN'
     ),
     'shipping_address' => array(
-        'first_name' => isset($transaction_details_row['first_name']) ? $transaction_details_row['first_name'] : '',
-        'address' => isset($transaction_details_row['alamat']) ? $transaction_details_row['alamat'] : '',
-        'city' => isset($transaction_details_row['kota']) ? $transaction_details_row['kota'] : '',
-        'postal_code' => isset($transaction_details_row['kopos']) ? $transaction_details_row['kopos'] : '',
-        'phone' => isset($transaction_details_row['phone']) ? $transaction_details_row['phone'] : '',
+        'first_name' => isset($customer_data_details['nama']) ? $customer_data_details['nama'] : '',
+        'address' => isset($customer_data_details['alamat']) ? $customer_data_details['alamat'] : '',
+        'city' => isset($customer_data_details['kota']) ? $customer_data_details['kota'] : '',
+        'postal_code' => isset($customer_data_details['kopos']) ? $customer_data_details['kopos'] : '',
+        'phone' => isset($customer_data_details['phone']) ? $customer_data_details['phone'] : '',
         'country_code' => 'IDN'
     )
 );
@@ -90,16 +97,11 @@ $transaction = array(
     'item_details' => $item_details,
 );
 
-// $snap_token = '';
 try {
     $snap_token = Snap::getSnapToken($transaction);
 } catch (\Exception $e) {
     echo $e->getMessage();
-    // komentar di atas di gunakan agar tidak menampilkan token di page
 }
-
-// echo "snapToken = " . $snap_token;
-// komentar di atas di gunakan agar tidak menampilkan token di page
 
 function printExampleWarningMessage()
 {
@@ -145,15 +147,11 @@ function printExampleWarningMessage()
             <div class="col-md-6 col-md-offset-3">
                 <h2 class="text-center">Checkout Berhasil!</h2>
                 <button id="pay-button" class="btn btn-lg btn-block"
-                    style="background-color: #611319; color: white;">Bayar
-                    Sekarang</button>
+                    style="background-color: #611319; color: white;">Bayar Sekarang</button>
                 <div id="result-json" class="result-item">
                     Status Pembayaran akan muncul disini:<br>
                 </div>
                 <br>
-                <!-- <button href="#" class="btn btn-lg btn-block mt-5"
-                    style="background-color: #ffe28f; color: black;">Kembali
-                    Beranda</button> -->
             </div>
         </div>
     </div>
